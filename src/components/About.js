88d4bab8fb68f01df402af3e5d8ff1476b757cb5
Christopher Cerretani <cerretani@gmail.com>
2022-08-25 07:39:14 -0400

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,7 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Hello, my name is Chris and I'm a community taught developer with a passion for learning, creating and problem solving.
-                I'm particularly interested in open-source, decentraliztion, blockchain, smart contracts and the future of web3.
+                Hello, my name is Chris. I'm a full-stack developer with a passion for continual learning, innovative tech, open source, blockchain and web3.
             </p>
          
         </section>
