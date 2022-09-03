4de5fddfdb1bdd0d3a86f72ead5e2451fd619e62
Christopher Cerretani <cerretani@gmail.com>
2022-09-03 11:37:00 -0400

    update app.js
    added wallet ConnectButton

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 // Import Components
 import Header from './components/Header';
 import About from './components/About';
@@ -5,11 +6,13 @@ import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Social from './components/Social';
 import Footer from './components/Footer';
+import ConnectButton from './components/ConnectButton';
 
 function App() {
 	return (
 		<div className="App">
-			<Header />
+			<ConnectButton />
+      <Header />
 			<Projects />
 			<About />
 			<Experience />
